5a76da03a003943fdcc3984fa8c9255903165629
iCaioReis <caioreis3186@gmail.com>
2023-11-13 23:21:13 -0300

    Middleware

--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -4,8 +4,14 @@ const UsersController = require ("../controllers/UsersController")
 
 const usersRoutes = Router();
 
+
+function middleware() {
+    console.log("Voce passou pelo middleware")
+}
+
+
 const usersController = new UsersController;
 
-usersRoutes.post("/users", usersController.create)
+usersRoutes.post("/users", middleware(),usersController.create)
 
 module.exports = usersRoutes;
