9e6a609b3d228f3aea336604ab6ed5907063949c
iCaioReis <caioreis3186@gmail.com>
2023-11-25 23:56:45 -0300

    Update Controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -21,6 +21,40 @@ class usersController {
 
         return res.status(201).json();
     }
+
+    async update(req, res){
+        const { name, email, password } = req.body;
+        const { id } = req.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        const checkUserExists = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!checkUserExists){
+            throw new AppError ("Usuário não encontrado!");
+        }
+
+        const checkEmailExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(checkEmailExists && checkEmailExists.id !== id){
+            throw new AppError ("E-mail já cadastrado!");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+        UPDATE users SET
+        name = ?,
+        email = ?,
+        updated_at = ?,
+        WHERE ID = ?`, 
+        [name, email, new Date(), id]);
+        return res.json();
+
+
+    }
 }
 
 module.exports = usersController;
