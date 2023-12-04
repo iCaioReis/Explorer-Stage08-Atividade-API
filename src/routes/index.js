43cd8c1c0b1c26a22d3900c2598ca18ba9f6ebc7
iCaioReis <caioreis3186@gmail.com>
2023-12-04 17:29:38 -0300

    Notes controller and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 
-const users = require("./users.route");
+const users = require("./users.routes");
+const notes = require("./notes.routes");
 
 //Incluir rotas aqui
 
 const routes = Router();
 
 routes.use('/users', users);
+routes.use('/notes', notes);
 
 module.exports = routes;
